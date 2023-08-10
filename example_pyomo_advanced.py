7beb0a02e66d6aea4a0a26dc626f245f9bac3f61
linanavas <67085421+linanavas@users.noreply.github.com>
2023-08-10 17:51:10 +0000

    add pyomo example

---
+++ example_pyomo_advanced.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+import pyomo.environ as pyo
+
+# from pyomo.environ import *
+from pyomo.opt import SolverFactory
+
+
+# Sets
+machines = range(1, 4 + 1)
+hours = range(1, 10 + 1)
+
+# Model
+model = pyo.ConcreteModel()
+
+# Variables
+model.x = pyo.Var(machines, hours, within=pyo.Integers, bounds=(0, 10))
+x = model.x
+
+# Objective function
+total_producton = sum([x[m, t] for m in machines for t in hours])
+model.obj = pyo.Objective(expr=total_producton, sense=pyo.maximize)
+
+# Constraint 1
+model.C1 = pyo.ConstraintList()
+for t in hours:
+    model.C1.add(expr=2 * x[2, t] - 8 * x[3, t] <= 0)
+
+# Constraint 2
+model.C2 = pyo.ConstraintList()
+for t in hours:
+    if t > 2:
+        model.C2.add(expr=x[2, t] - 2 * x[3, t - 2] + x[4, t] >= 1)
+
+# Constraint 3
+model.C3 = pyo.ConstraintList()
+for t in hours:
+    model.C3.add(expr=sum([x[m, t] for m in machines]) <= 50)
+
+# Constraint 4
+model.C4 = pyo.ConstraintList()
+for t in hours:
+    if t > 1:
+        model.C4.add(expr=x[1, t] + x[2, t - 1] + x[3, t] + x[4, t] <= 10)
+
+# Solver
+
+opt = SolverFactory("glpk")
+results = opt.solve(model)
+
+# model.pprint()
+# Print results
+
+for m in machines:
+    for t in hours:
+        print("x" + str(m) + "," + str(t) + " = " + str(pyo.value(x[m, t])))
+
+print()
+print("Production = ", str(pyo.value(model.obj)))
